18289ad7c368715f2a9a71799c0f77b9f86ed679
trumbitta <trumbitta@gmail.com>
2012-06-11 21:17:26 +0200

    add default content model placeholder for lists

--- src/main/resources/archetype-resources/src/main/sql/postgresql/port.sql
+++ src/main/resources/archetype-resources/src/main/sql/postgresql/port.sql
@@ -2,5 +2,7 @@
 -- change the x to something suitable and unused
 -- set the last column to NULL if your Content Model doesn't need any stylesheet at all
 -- You'll want to provide at least one Default Content Model, and one Content Model for lists
-INSERT INTO contentmodels (modelid, contenttype, descr, model, stylesheet) VALUES (1000x, '${content-three-caps-letters-code}', 'My Content Model', 'here the content model velocity snippet', 'contentmodels/${content-three-caps-letters-code}.css');
+INSERT INTO contentmodels (modelid, contenttype, descr, model, stylesheet) VALUES (1000x, '${content-three-caps-letters-code}', 'My Content Model - Default', 'here the content model velocity snippet', 'contentmodels/${content-three-caps-letters-code}.css');
+
+INSERT INTO contentmodels (modelid, contenttype, descr, model, stylesheet) VALUES (100x1, '${content-three-caps-letters-code}', 'My Content Model - Lists', 'here the content model velocity snippet', 'contentmodels/${content-three-caps-letters-code}.css');
 
